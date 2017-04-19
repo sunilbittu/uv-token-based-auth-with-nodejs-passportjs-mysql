c19db81e24968ebf0cf775ae0228fd19eafbef71
Yuvaraj <shivarajnaidu@gmail.com>
2017-04-19 13:04:52 +0530

    Added logic to create users table automatically if it's not already exist

--- app/db/lib/pool.js
+++ app/db/lib/pool.js
@@ -2,21 +2,51 @@
 const mysql    = require('mysql');
 const dbConfig = require('../../../config/index').DB;
 const pool     = mysql.createPool(dbConfig);
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isNotProduction = (NODE_ENV !== 'production');
 
-// pool.query('SHOW TABLES', (error, results) => {
-// 	if(error) return console.log(error);
-// 	console.log(results)
-// });
+function createUsersTable () {
 
-if (process.env.NODE_ENV === 'development') {
-	pool.on('connection', function (connection) {
-		console.log('%d conneted', connection.threadId)
-	});
+	const SQLQuery = `
+	CREATE TABLE user (
+	id int(11) NOT NULL AUTO_INCREMENT,
+	name varchar(100),
+	email varchar(100),
+	password varchar(500),
+	fbid varchar(300),
+	fbname varchar(100),
+	PRIMARY KEY (id)
+	)`;
+   // console.log(SQLQuery)
+   if (isNotProduction) console.log('Creating New Table');
 
-	pool.on('release', function (connection) {
-		console.log('Connection %d released', connection.threadId);
-	});
+   pool.query(SQLQuery, (error, results) => {
+   	if(error) return console.log(error);
+   	const isTableCreated = (results.serverStatus === 2);
+   	if (isNotProduction && isTableCreated)  console.log('Table Created Successfully');
+   });
 }
 
+
+function checkWhetherTableExist (error, results) {
+	if(error) return console.log(error);
+	const isTableNotExist = (results.length === 0);
+
+	if (isTableNotExist) {
+		if (isNotProduction) console.log('users table not exist');
+		createUsersTable();
+	}
+
+}
+
+pool.query('SHOW TABLES LIKE "users"', checkWhetherTableExist);
+
+pool.on('connection', function (connection) {
+	console.log('%d conneted', connection.threadId)
+});
+
+pool.on('release', function (connection) {
+	console.log('Connection %d released', connection.threadId);
+});
 
 module.exports = pool;
