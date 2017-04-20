dd6559c618d5a3c689a6a846783a4f6432948c23
Yuvaraj <shivarajnaidu@gmail.com>
2017-04-20 13:44:26 +0530

    host in prod conf changed to 127.0.0.1

--- config/production/db.js
+++ config/production/db.js
@@ -2,7 +2,7 @@
 
 const dbConfig = {
     connectionLimit: 100,
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root', //Provide Your DB username
     password: '', //Provide Your DB password
     database: 'test'  //Provide Your DB Name
